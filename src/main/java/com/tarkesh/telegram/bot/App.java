34eb02370e89adc8097cda1f50237cb350b7b826
Tarkeshwer Kumar <tarkesh4338@gmail.com>
2022-11-09 11:49:36 +0530

    procfile added

--- src/main/java/com/tarkesh/telegram/bot/App.java
+++ src/main/java/com/tarkesh/telegram/bot/App.java
@@ -5,12 +5,16 @@ import org.apache.commons.lang3.StringUtils;
 public class App {
   public static void main(String[] args) {
 
+    System.out.println("Getting token ");
+
     String botToken = System.getenv("BOT_TOKEN");
 
     if (StringUtils.isEmpty(botToken)) {
       System.out.println("Set bot token as environment variable, Varibale name - BOT_TOKEN ");
       System.exit(0);
     }
+
+    System.out.println("Token Recieved , Starting bot");
 
     QuoteBot quoteBot = new QuoteBot(botToken);
 
